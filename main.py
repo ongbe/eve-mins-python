83022db63c185522e3bfce5dab7fc2dd4fd2e30c
linduxed <linduxed@gmail.com>
2010-03-31 12:50:00 +0200

    Spaces to tabs, and price input choice is now a method.

--- main.py
+++ main.py
@@ -11,89 +11,93 @@ locale.setlocale(locale.LC_ALL, ('sv_SE', 'UTF8'))
 
 # Checks for empty strings.
 def get_int_input(str):
-    try:
-        return int(str)
-    except ValueError:
-        return 0
+	try:
+		return int(str)
+	except ValueError:
+		return 0
 
 def get_float_input(str):
-    try:
-        return float(str)
-    except ValueError:
-        return 0
+	try:
+		return float(str)
+	except ValueError:
+		return 0
 
 def check_choice(str):
-    if str in ['y', 'Y', 'n', 'N']:
-        return True
-    else:
-        return False
+	if str in ['y', 'Y', 'n', 'N']:
+		return True
+	else:
+		return False
 
 def check_loop(str):
-    while True:
-        if check_choice(str):
-            return str
-        else:
-            str = input("Your input was faulty. Load predefined? (Y/N) ")
+	while True:
+		if check_choice(str):
+			return str
+		else:
+			str = input("Your input was faulty. Load predefined? (Y/N) ")
 
 def load_prices():
-    mineralPrices = {}
-    rawPrices = open('prices.list')
+	mineralPrices = {}
+	rawPrices = open('prices.list')
 
-    line = rawPrices.readline()
-    while line != "":
-        splitLine = line.split("=")
-        mineralPrices[splitLine[0].lower()] = splitLine[1].strip()
-        line = rawPrices.readline()
-    return mineralPrices
+	line = rawPrices.readline()
+	while line != "":
+		splitLine = line.split("=")
+		mineralPrices[splitLine[0].lower()] = splitLine[1].strip()
+		line = rawPrices.readline()
+	return mineralPrices
 
 def manual_prices():
-    mineralPrices = {}
+	mineralPrices = {}
 
-    print("Please enter the price of each mineral.")
-    mineralPrices['tritanium'] = get_float_input(input("Tritanium: "))
-    mineralPrices['pyerite'] = get_float_input(input("Pyerite: "))
-    mineralPrices['mexallon'] = get_float_input(input("Mexallon: "))
-    mineralPrices['isogen'] = get_float_input(input("Isogen: "))
-    mineralPrices['nocxium'] = get_float_input(input("Nocxium: "))
-    mineralPrices['zydrine'] = get_float_input(input("Zydrine: "))
-    mineralPrices['megacyte'] = get_float_input(input("Megacyte: "))
-    
-    return mineralPrices
+	print("Please enter the price of each mineral.")
+	mineralPrices['tritanium'] = get_float_input(input("Tritanium: "))
+	mineralPrices['pyerite'] = get_float_input(input("Pyerite: "))
+	mineralPrices['mexallon'] = get_float_input(input("Mexallon: "))
+	mineralPrices['isogen'] = get_float_input(input("Isogen: "))
+	mineralPrices['nocxium'] = get_float_input(input("Nocxium: "))
+	mineralPrices['zydrine'] = get_float_input(input("Zydrine: "))
+	mineralPrices['megacyte'] = get_float_input(input("Megacyte: "))
+	
+	return mineralPrices
 
 def manual_amounts():
-    mineralAmounts = {}
-    mineralAmounts['tritanium'] = get_int_input(input(
-        "How much Tritanium was refined? "))
-    mineralAmounts['pyerite'] = get_int_input(input(
-        "How much Pyerite was refined? "))
-    mineralAmounts['mexallon'] = get_int_input(input(
-        "How much Mexallon was refined? "))
-    mineralAmounts['isogen'] = get_int_input(input(
-        "How much Isogen was refined? "))
-    mineralAmounts['nocxium'] = get_int_input(input(
-        "How much Nocxium was refined? "))
-    mineralAmounts['zydrine'] = get_int_input(input(
-        "How much Zydrine was refined? "))
-    mineralAmounts['megacyte'] = get_int_input(input(
-        "How much Megacyte was refined? "))
+	mineralAmounts = {}
+	mineralAmounts['tritanium'] = get_int_input(input(
+		"How much Tritanium was refined? "))
+	mineralAmounts['pyerite'] = get_int_input(input(
+		"How much Pyerite was refined? "))
+	mineralAmounts['mexallon'] = get_int_input(input(
+		"How much Mexallon was refined? "))
+	mineralAmounts['isogen'] = get_int_input(input(
+		"How much Isogen was refined? "))
+	mineralAmounts['nocxium'] = get_int_input(input(
+		"How much Nocxium was refined? "))
+	mineralAmounts['zydrine'] = get_int_input(input(
+		"How much Zydrine was refined? "))
+	mineralAmounts['megacyte'] = get_int_input(input(
+		"How much Megacyte was refined? "))
 
-    return mineralAmounts
+	return mineralAmounts
+
+# User is given choice to either load predefined mineral prices or input them
+# manually.
+def price_input_method_choice():
+	priceChoice = input("Do you wish to load predifined mineral prices? (Y/N) ")
+	if check_loop(priceChoice) in ['y', 'Y']:
+		print("\nPrices loaded.\n")
+		mineralPrices = load_prices()
+	else:
+		print("\nManual input selected.\n")
+		mineralPrices = manual_prices()
 
 
 # -----------------------------
 # User interaction starts here.
 # -----------------------------
 
-# User is given choice to either load predefined mineral prices or input
-# them manually.
-priceChoice = input("Do you wish to load predifined mineral prices? (Y/N) ")
-if check_loop(priceChoice) in ['y', 'Y']:
-    mineralPrices = load_prices()
-else:
-    print("\nManual input selected.\n")
-    mineralPrices = manual_prices()
+price_input_method_choice()
 
-# Get amounts of minerals.
+# TODO: Give option to load predefined amounts from file.
 mineralAmounts = manual_amounts()
 
 # Creates a list and appends the minerals (name, amount, price) onto it.
